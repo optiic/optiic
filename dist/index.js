1202e4a70a0ef9b099aa3e7da11787be9c8ddee4
Ian <ian.wiedenman@gmail.com>
2022-06-08 21:00:06 -0700

    Added prepare-package

--- dist/index.js
+++ dist/index.js
@@ -22,7 +22,7 @@
   var isRemoteURL = /^https?:\/\/|^\/\//i;
   var CONTENT_JSON = 'application/json';
   var SOURCE = 'library';
-  var VERSION = '0.0.17';
+  var VERSION = '0.0.18';
 
   function Optiic(options) {
     options = options || {};
--- src/prepare.js
+++
@@ -1,7 +0,0 @@
-const jetpack = require('fs-jetpack');
-const package = require('../package.json');
-
-jetpack.write(
-  './dist/index.js',
-  jetpack.read('./src/index.js').replace(/{version}/igm, package.version),
-)
