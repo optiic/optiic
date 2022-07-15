39ee98a00b2b299446a238ac564896d33a7d4aef
Ian <ian.wiedenman@gmail.com>
2022-07-15 16:08:12 -0700

    Updated

---
+++ test/test.js
@@ -0,0 +1,30 @@
+const assert = require('assert');
+
+beforeEach(() => {
+});
+
+before(() => {
+});
+
+after(() => {
+});
+
+/*
+ * ============
+ *  Test Cases
+ * ============
+ */
+describe('Sniips', () => {
+  const lib = require('../dist/index.js');
+
+  // Method
+  describe('.method()', () => {
+
+    // Method tests
+    it('this => that', () => {
+      return assert.equal(lib, {});
+    });
+
+  });
+
+})
