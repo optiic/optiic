6b01437297c3a3fcd92c64520de423477b746427
Ian <ian.wiedenman@gmail.com>
2022-10-02 20:00:40 -0700

    Added .npmignore and removed files

--- dist/index.js
+++ dist/index.js
@@ -22,7 +22,7 @@
   var isRemoteURL = /^https?:\/\/|^\/\//i;
   var CONTENT_JSON = 'application/json';
   var SOURCE = 'library';
-  var VERSION = '0.0.20';
+  var VERSION = '0.0.21';
 
   function Optiic(options) {
     options = options || {};
